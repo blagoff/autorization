44eccca3eadf83f84b4f731fbb370cd2ad34c579
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-03-06 23:23:09 +0100

    error 400

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID' : '941246771901-1ng2msj3v6tob4stbpcl3qu1tfulvfkv.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET' : 'aABKAn1E13k7rry9xM0oReRo',
+	'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
+};
